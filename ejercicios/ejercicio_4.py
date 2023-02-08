ed03138c97a05ead22fe5a221e8fcc6faa855fbe
JuanPa28 <juanpramirez18@gmail.com>
2023-02-07 20:04:31 -0500

    Commit exercise 4

---
+++ ejercicios/ejercicio_4.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    num=int(input("Digite el numero a evaluar: "))
+    def numeros_pares_o_impares(n):
+        if n % 2 == 0:
+            print("\nEl numero es par")
+        else:
+            print("\nEl numero es impar")
+
+numeros_pares_o_impares(num)
