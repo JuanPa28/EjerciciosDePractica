d2ab57eb165cc174cf7bc7583af0b03b4c2f3934
JuanPa28 <juanpramirez18@gmail.com>
2023-02-07 19:38:21 -0500

    Commit exercise 1

---
+++ ejercicios/ejercicio_1.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    name = input("Digite su nombre: ")
+    last_name = input("\nDigite su appellido: ")
+
+    age = int(input("\nDigite su edad: "))
+
+    print(f"\nBuen dia {name} {last_name}, espero que se encuentre muy bien con sus {age} años de edad")
+
