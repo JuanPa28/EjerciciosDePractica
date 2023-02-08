4a5ebd9d49a4e63111111e2f9d2b97f6ca84fab2
JuanPa28 <juanpramirez18@gmail.com>
2023-02-07 21:14:39 -0500

    Commit exercise 13

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,13 @@
+if __name__=="__main__":
+    num1=float(input("Digite el primer numero: "))
+    num2= float(input("Digite el segundo numero: "))
+
+    sum=num1+num2
+    res=num1-num2
+    multi=num1*num2
+    div=num1/num2
+
+    print(f"La suma de los numeros {num1} y {num2} es = a {sum}")
+    print(f"La resta de los numeros {num1} y {num2} es = a {res}")
+    print(f"La multiplicacion de los numeros {num1} y {num2} es = a {multi}")
+    print(f"La division de los numeros {num1} y {num2} es = a {div}")
